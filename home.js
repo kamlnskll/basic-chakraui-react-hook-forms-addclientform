504f8b7ecc243a0011dff5bd4bd6a936a824e08a
kamlnskll <91806277+kamlnskll@users.noreply.github.com>
2022-02-23 16:22:40 -0600

    Update home.js

--- home.js
+++ home.js
@@ -2,9 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import { FaSun, FaMoon } from 'react-icons/fa'
 import {
-  Box,
   Flex,
-  Heading,
   useColorMode,
   VStack,
   IconButton,
